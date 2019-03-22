f5b8c8c8464c15b6e31ab2eda13a2cc4e6340802
JulioMegia1 <julio.o.mm@hotmail.com>
2019-03-22 14:27:55 -0600

    a

--- src/pages/pantallaprincipal/pantallaprincipal.ts
+++ src/pages/pantallaprincipal/pantallaprincipal.ts
@@ -187,7 +187,7 @@ export class PantallaprincipalPage {
           datasets: [
               {
                   label: "My First dataset",
-                  fill: false,
+                  fill: false,  
                   lineTension: 0.1,
                   backgroundColor: "rgba(75,192,192,0.4)",
                   borderColor: "rgba(75,192,192,1)",
